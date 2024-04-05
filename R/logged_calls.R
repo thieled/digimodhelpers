5170563ac8c0548f9c5341a515f007601714ffe1
Daniel Thiele <daniel.thiele@fu-berlin.de>
2024-04-05 15:26:36 +0200

    revised yt functions, add call_log_yt

--- R/logged_calls.R
+++ R/logged_calls.R
@@ -97,3 +97,186 @@ call_log_ct <- function(grid_df,
 
   return(results)
 }
+
+
+
+
+
+#' Call YouTube API for Multiple Accounts and Store Results
+#'
+#' This function calls the YouTube API for multiple accounts specified in a grid dataframe and stores the results in JSON format.
+#'
+#' @param grid_df A dataframe containing the grid of parameters for each account, including 'data' for the data directory,
+#'  'accounts' for the account IDs, 'start' for the start date, and 'end' for the end date.
+#' @param traceback Logical. If TRUE, includes a traceback in error messages. Defaults to FALSE.
+#' @param autolog Logical. If TRUE, automatically logs messages to a file. Defaults to TRUE.
+#' @param show_notes Logical. If TRUE, shows notes in the log file. Defaults to TRUE.
+#' @param compact Logical. If TRUE, uses compact formatting for log messages. Defaults to FALSE.
+#' @param progress_bar Logical. If TRUE, displays a progress bar. Defaults to TRUE.
+#' @param return_results Logical. If TRUE, returns the results. Defaults to TRUE.
+#' @param verbose Logical. If TRUE, displays verbose messages. Defaults to TRUE.
+#'
+#' @return A list containing the results for each account, or NULL if an error occurs.
+#'
+#' @details This function iterates over each row of the grid dataframe and calls the `get_channel_vids_ERRORS` function from the 'tuber' package
+#' to retrieve video details for each account within the specified date range. Results are stored in JSON format in the data directory specified
+#' in the grid dataframe. Error messages and warnings are logged to a file.
+#'
+#' @examples
+#' \dontrun{
+#' # Create a grid dataframe
+#' grid_df <- data.frame(
+#'   data = "data_directory",
+#'   accounts = c("account1", "account2"),
+#'   start = "2022-01-01",
+#'   end = "2022-01-31"
+#' )
+#'
+#' # Call the YouTube API for multiple accounts
+#' call_log_yt(grid_df)
+#' }
+#'
+#' @export
+call_log_yt <- function(grid_df,
+                        traceback = FALSE,
+                        autolog = TRUE,
+                        show_notes = TRUE,
+                        compact = FALSE,
+                        progress_bar = TRUE,
+                        return_results = TRUE,
+                        verbose = TRUE) {
+
+  # Check if data dir exists, create if not
+  if(!dir.exists(grid_df$data[[1]])){
+    dir.create(file.path(grid_df$data[[1]]), recursive = TRUE, showWarnings = FALSE)
+  }
+
+  # Define log file
+  now <- format(Sys.time(), "%Y-%m-%d_%H-%M-%S")
+  dir.create(file.path(grid_df$data[[1]], "/log"),  recursive = TRUE, showWarnings = FALSE)
+  log_file <- paste0(grid_df$data[[1]], "/log", "/call_log_yt_", now, ".log")
+
+  # Set up logging
+  logger::log_appender(logger::appender_file(log_file))
+  logger::log_info("Logging started.")
+
+  # Define a function to handle each row of the grid
+  process_row <- function(accounts,
+                          start,
+                          end,
+                          filename,
+                          data) {
+    # Log row information
+    if(verbose){
+      message(paste0(
+        "Calling YT: Account - ",
+        accounts,
+        ", Start - ",
+        start,
+        ", End - ",
+        end
+      ))
+    }
+
+    logger::log_info(paste0(
+      "Calling YT: Account - ",
+      accounts,
+      ", Start - ",
+      start,
+      ", End - ",
+      end
+    ))
+
+    result <- tryCatch(
+      {
+        # Call the API
+        get_channel_vids(
+          channel_id = accounts,
+          start_date = start,
+          end_date = end
+        )
+      },
+      error = function(e) {
+        # Log errors
+        if(verbose){message(e$message)}
+        logger::log_error(paste("Error in:", filename, "Message:", e$message))
+        # Return NULL in case of error
+        return(NULL)
+      },
+      warning = function(w) {
+        if(verbose){message(w$message)}
+        logger::log_warn(paste("Warning in", filename, "Message:", w$message))
+        return(NULL)
+      }
+    )
+
+    ## Store results if not empty
+    if(!is.null(result) && length(result) > 0){
+
+      # define file path
+      file <- paste0(
+        data,
+        "/",
+        filename,
+        sub("\\:", "m", sub("\\:", "h", lubridate::now(tzone = "UTC") |> lubridate::format_ISO8601())),
+        ".json"
+      )
+
+      # Print message and log info
+      if(verbose){message(paste0("Entries fetched: n = ", length(result), ". Saving to - ", file))}
+      logger::log_info(paste0("Entries fetched: n = ", length(result), ". Saving to - ", file))
+
+      # Convert to json
+      js <- jsonlite::toJSON(result)
+      write(x = js, file = file)
+
+      return(result)  # Return the result
+
+    }else{
+      # Print message and log info
+      if(verbose){message(paste0("Entries fetched: n = ", 0, ". Not saving."))}
+      logger::log_info(paste0("Entries fetched: n = ", 0, ". Not saving."))
+      return(NULL)
+    }
+
+
+  }
+
+
+  # Wrap function in safely
+  process_row_safe <- purrr::safely(process_row, otherwise = NULL)
+
+  # Use purrr::pmap to iterate over the rows of the grid
+  tryCatch({
+    results <- purrr::pmap(grid_df, .progress = progress_bar, function(...) {
+
+      result <- process_row_safe(...)
+
+      if (!is.null(result$error)) {
+        if(verbose){message(paste("Error in 'get_channel_vids.' Message:", result$error$message, ". NOTE: Invalid account ID?"))}
+        logger::log_error(paste("Error in 'get_channel_vids.' Message:", result$error$message, ". NOTE: Invalid account ID?"))
+      }
+
+      return(result$result
+      ) # Return the result portion of the list
+    })
+  }, error = function(e) {
+    # Log errors
+    if(verbose){message(paste("Error in calling 'purrr::pmap.' Message:", e$message))}
+    logger::log_error(paste("Error in calling 'purrr::pmap.' Message:", e$message))
+    # Return NULL in case of error
+    return(NULL)
+  })
+
+  # Close the logging
+  logger::log_info("Logging completed.")
+  logger::log_appender(NULL)
+
+  if(return_results){
+    return(results)
+  }
+
+
+}
+
+
--- R/tuber_utils.R
+++ R/tuber_utils.R
@@ -1,59 +1,68 @@
-#' Get Video Details from YouTube Channel
+#' Retrieve video details from a YouTube channel
 #'
-#' This function retrieves video details from a YouTube channel based on provided parameters.
+#' This function retrieves video details from a YouTube channel, including information such as video ID, title, description, and statistics.
 #'
-#' @param channel_id The ID of the YouTube channel.
-#' @param start_date The start date to filter videos. Defaults to NULL (no filtering).
-#' @param end_date The end date to filter videos. Defaults to NULL (no filtering).
-#' @param max_results Maximum number of results to retrieve. Defaults to Inf (retrieve all results).
-#' @param part A character vector specifying the parts of the resource to retrieve.
-#' Defaults to c("contentDetails", "id", "liveStreamingDetails", "localizations",
-#' "player", "recordingDetails", "snippet", "statistics", "status", "topicDetails").
-#' @param ... Additional arguments to be passed to underlying functions.
-#'
-#' @return A list containing details of videos from the YouTube channel.
-#'
+#' @param channel_id The ID of the YouTube channel from which to retrieve video details.
+#' @param start_date Optional. The start date to filter videos by their published date. Defaults to NULL.
+#' @param end_date Optional. The end date to filter videos by their published date. Defaults to NULL.
+#' @param max_results Optional. Maximum number of video details to retrieve. Defaults to Inf.
+#' @param part Optional. A character vector specifying the parts that the API response will include. Possible values are: "contentDetails", "id", "liveStreamingDetails", "localizations", "player", "recordingDetails", "snippet", "statistics", "status", "topicDetails". Defaults to include all parts.
+#' @param auth Optional. The authentication method to be used. Defaults to "key".
+#' @param ... Additional arguments passed to tuber_GET.
+#' @return A list containing video details.
+#' @details This function first retrieves channel resources using the `list_channel_resources` function from the 'tuber' package. It then extracts the playlist ID and retrieves playlist items using the `get_playlist_items` function. Finally, it retrieves details for each video using the `get_video_details` function. If no videos are found or an error occurs during the process, NULL is returned.
 #' @examples
 #' \dontrun{
-#' get_channel_vids(channel_id = "your_channel_id", start_date = "2023-01-01", end_date = "2023-12-31")
+#' # Authenticate using API key
+#' tuber::yt_set_key("YOUR_API_KEY")
+#'
+#' # Retrieve video details for a channel
+#' videos <- get_channel_vids(channel_id = "CHANNEL_ID", max_results = 10)
 #' }
-#'
 #' @export
-#'
 
 get_channel_vids <- function(channel_id = channel_id,
                              start_date = NULL,
                              end_date = NULL,
                              max_results = Inf,
                              part = c("contentDetails", "id", "liveStreamingDetails", "localizations", "player", "recordingDetails", "snippet", "statistics", "status", "topicDetails"),
+                             auth = "key",
                              ...){
 
   channel_res <- tryCatch({
-    tuber::list_channel_resources(filter = list(channel_id = channel_id), part="contentDetails")
+    tuber::list_channel_resources(filter = list(channel_id = channel_id), part="contentDetails", auth = auth)
   }, error = function(e) {
-    message(paste0("Error when calling 'list_channel_resources' on ", channel_id, ": ", e))
+    stop(paste0("Error when calling 'list_channel_resources' on ", channel_id, ": ", e))
     # Return NULL in case of error
     return(NULL)
   }
   )
 
+  if (inherits(channel_res, "response")) {
+
+    ## TO Do: implement automatic re-authentification here
+
+    stop(paste("Error when calling 'list_channel_resources' on ", channel_id, ": HTTP failure:", channel_res$status_code))
+  }
+
+  if (channel_res$pageInfo$totalResults == 0) {
+    stop(paste0("Error: No results for ", channel_id, ". Invalid channel ID?"))
+  }
+
   # Extract playlist ID
-  if(!is.null(channel_res)){
+  if (!is.null(channel_res)) {
     # Uploaded playlists:
     playlist_id <- channel_res$items[[1]]$contentDetails$relatedPlaylists$uploads
 
-    playlist_items <-   tryCatch({
-      tuber::get_playlist_items(filter = list(playlist_id = playlist_id), max_results = max_results, simplify = TRUE)
+    playlist_items <- tryCatch({
+      tuber::get_playlist_items(filter = list(playlist_id = playlist_id), max_results = max_results, simplify = TRUE, auth = auth)
     }, error = function(e) {
-      message(paste0("Error when calling 'get_playlist_items' on ", playlist_id, ": ", e))
-      # Return NULL in case of error
-      return(NULL)
+      stop(paste0("Error when calling 'get_playlist_items' on ", playlist_id, ": ", e))
     }
     )
 
-
     # Subset items to time frame
-    if(!is.null(playlist_items)  && (!is.null(start_date) || !is.null(end_date))){
+    if(!is.null(playlist_items) && (!is.null(start_date) || !is.null(end_date))){
 
       playlist_items$contentDetails.videoPublishedAt <- lubridate::as_datetime(playlist_items$contentDetails.videoPublishedAt)
 
@@ -70,36 +79,135 @@ get_channel_vids <- function(channel_id = channel_id,
     # Get video IDs
     if(!is.null(playlist_items)){
       vid_ids <- playlist_items$contentDetails.videoId
+      n <- length(vid_ids)
     }
 
 
     # Error-proof function to get video details
-    trycatch_get_video_details <- function(video_id, part = "snippet"){
+    trycatch_get_video_details <- function(video_id, part = part, auth = auth){
 
       v_det <- tryCatch({
-        details <- tuber::get_video_details(video_id = video_id, part = part)
-        details <- c(details,
-                     download_time = as.character(as.POSIXct(lubridate::now(), tz = "UTC")),
-                     download_time_zone = "UTC",
-                     download_software = "tuber",
-                     yt_api = "v3")
-        return(details)
+        tuber::get_video_details(video_id = video_id, part = part, auth = auth)
       }, error = function(e) {
-        message(paste0("Error when calling 'get_playlist_items' on ", vid_ids, ": ", e))
-        # Return NULL in case of error
+        stop(paste0("Error when calling 'get_playlist_items' on ", vid_ids, ": ", e))
+        return(NULL)
+      }, warning = function(w) {
+        warning(paste0("Warning when calling 'get_playlist_items' on ", vid_ids, ": ", w))
         return(NULL)
       }
       )
+
+      if(!is.null(v_det)){
+
+        v_det <- c(v_det$items[[1]],
+                   download_time = as.character(as.POSIXct(lubridate::now(), tz = "UTC")),
+                   download_time_zone = "UTC",
+                   download_software = "tuber",
+                   yt_api = "v3")
+
+      }
+
+      return(v_det)
+
     }
 
     # Get video details
-    n <- length(vid_ids)
     message(paste0("Fetching details from n = ", n, " videos on channel_id: ", channel_id, " ..."))
-    details <- pbapply::pblapply(vid_ids, trycatch_get_video_details, part = part)
+    details <- pbapply::pblapply(vid_ids, trycatch_get_video_details, part = part, auth = auth)
 
     return(details)
 
-  }else{
+  } else {
+    message("No playlist items found.")
     return(NULL)
   }
+
 }
+
+
+
+
+# Retired version that does not return errors properly:
+#
+# get_channel_vids <- function(channel_id = channel_id,
+#                              start_date = NULL,
+#                              end_date = NULL,
+#                              max_results = Inf,
+#                              part = c("contentDetails", "id", "liveStreamingDetails", "localizations", "player", "recordingDetails", "snippet", "statistics", "status", "topicDetails"),
+#                              ...){
+#
+#   channel_res <- tryCatch({
+#     tuber::list_channel_resources(filter = list(channel_id = channel_id), part="contentDetails")
+#   }, error = function(e) {
+#     message(paste0("Error when calling 'list_channel_resources' on ", channel_id, ": ", e))
+#     # Return NULL in case of error
+#     return(NULL)
+#   }
+#   )
+#
+#   # Extract playlist ID
+#   if(!is.null(channel_res)){
+#     # Uploaded playlists:
+#     playlist_id <- channel_res$items[[1]]$contentDetails$relatedPlaylists$uploads
+#
+#     playlist_items <-   tryCatch({
+#       tuber::get_playlist_items(filter = list(playlist_id = playlist_id), max_results = max_results, simplify = TRUE)
+#     }, error = function(e) {
+#       message(paste0("Error when calling 'get_playlist_items' on ", playlist_id, ": ", e))
+#       # Return NULL in case of error
+#       return(NULL)
+#     }
+#     )
+#
+#
+#     # Subset items to time frame
+#     if(!is.null(playlist_items)  && (!is.null(start_date) || !is.null(end_date))){
+#
+#       playlist_items$contentDetails.videoPublishedAt <- lubridate::as_datetime(playlist_items$contentDetails.videoPublishedAt)
+#
+#       if(!is.null(start_date)){
+#         playlist_items <- subset(playlist_items, contentDetails.videoPublishedAt > lubridate::as_datetime(start_date))
+#       }
+#
+#       if(!is.null(end_date)){
+#         playlist_items <- subset(playlist_items, contentDetails.videoPublishedAt > lubridate::as_datetime(end_date))
+#       }
+#
+#     }
+#
+#     # Get video IDs
+#     if(!is.null(playlist_items)){
+#       vid_ids <- playlist_items$contentDetails.videoId
+#     }
+#
+#
+#     # Error-proof function to get video details
+#     trycatch_get_video_details <- function(video_id, part = "snippet"){
+#
+#       v_det <- tryCatch({
+#         details <- tuber::get_video_details(video_id = video_id, part = part)
+#         details <- c(details,
+#                      download_time = as.character(as.POSIXct(lubridate::now(), tz = "UTC")),
+#                      download_time_zone = "UTC",
+#                      download_software = "tuber",
+#                      yt_api = "v3")
+#         return(details)
+#       }, error = function(e) {
+#         message(paste0("Error when calling 'get_playlist_items' on ", vid_ids, ": ", e))
+#         # Return NULL in case of error
+#         return(NULL)
+#       }
+#       )
+#     }
+#
+#     # Get video details
+#     n <- length(vid_ids)
+#     message(paste0("Fetching details from n = ", n, " videos on channel_id: ", channel_id, " ..."))
+#     details <- pbapply::pblapply(vid_ids, trycatch_get_video_details, part = part)
+#
+#     return(details)
+#
+#   }else{
+#     return(NULL)
+#   }
+# }
