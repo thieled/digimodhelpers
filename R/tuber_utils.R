62b631097162bd161da67e9edb5d36df983d5398
thieled <daniel.thiele@univie.ac.at>
2024-04-10 14:44:41 +0200

    adding preliminary tuber::get_playlist_items fix

--- R/tuber_utils.R
+++ R/tuber_utils.R
@@ -55,7 +55,7 @@ get_channel_vids <- function(channel_id = channel_id,
     playlist_id <- channel_res$items[[1]]$contentDetails$relatedPlaylists$uploads
 
     playlist_items <- tryCatch({
-      tuber::get_playlist_items(filter = list(playlist_id = playlist_id), max_results = max_results, simplify = TRUE, auth = auth)
+      get_playlist_items_FIX(filter = list(playlist_id = playlist_id), max_results = max_results, simplify = TRUE, auth = auth)
     }, error = function(e) {
       stop(paste0("Error when calling 'get_playlist_items' on ", playlist_id, ": ", e))
     }
@@ -127,87 +127,99 @@ get_channel_vids <- function(channel_id = channel_id,
 
 
 
-# Retired version that does not return errors properly:
-#
-# get_channel_vids <- function(channel_id = channel_id,
-#                              start_date = NULL,
-#                              end_date = NULL,
-#                              max_results = Inf,
-#                              part = c("contentDetails", "id", "liveStreamingDetails", "localizations", "player", "recordingDetails", "snippet", "statistics", "status", "topicDetails"),
-#                              ...){
-#
-#   channel_res <- tryCatch({
-#     tuber::list_channel_resources(filter = list(channel_id = channel_id), part="contentDetails")
-#   }, error = function(e) {
-#     message(paste0("Error when calling 'list_channel_resources' on ", channel_id, ": ", e))
-#     # Return NULL in case of error
-#     return(NULL)
-#   }
-#   )
-#
-#   # Extract playlist ID
-#   if(!is.null(channel_res)){
-#     # Uploaded playlists:
-#     playlist_id <- channel_res$items[[1]]$contentDetails$relatedPlaylists$uploads
-#
-#     playlist_items <-   tryCatch({
-#       tuber::get_playlist_items(filter = list(playlist_id = playlist_id), max_results = max_results, simplify = TRUE)
-#     }, error = function(e) {
-#       message(paste0("Error when calling 'get_playlist_items' on ", playlist_id, ": ", e))
-#       # Return NULL in case of error
-#       return(NULL)
-#     }
-#     )
-#
-#
-#     # Subset items to time frame
-#     if(!is.null(playlist_items)  && (!is.null(start_date) || !is.null(end_date))){
-#
-#       playlist_items$contentDetails.videoPublishedAt <- lubridate::as_datetime(playlist_items$contentDetails.videoPublishedAt)
-#
-#       if(!is.null(start_date)){
-#         playlist_items <- subset(playlist_items, contentDetails.videoPublishedAt > lubridate::as_datetime(start_date))
-#       }
-#
-#       if(!is.null(end_date)){
-#         playlist_items <- subset(playlist_items, contentDetails.videoPublishedAt > lubridate::as_datetime(end_date))
-#       }
-#
-#     }
-#
-#     # Get video IDs
-#     if(!is.null(playlist_items)){
-#       vid_ids <- playlist_items$contentDetails.videoId
-#     }
-#
-#
-#     # Error-proof function to get video details
-#     trycatch_get_video_details <- function(video_id, part = "snippet"){
-#
-#       v_det <- tryCatch({
-#         details <- tuber::get_video_details(video_id = video_id, part = part)
-#         details <- c(details,
-#                      download_time = as.character(as.POSIXct(lubridate::now(), tz = "UTC")),
-#                      download_time_zone = "UTC",
-#                      download_software = "tuber",
-#                      yt_api = "v3")
-#         return(details)
-#       }, error = function(e) {
-#         message(paste0("Error when calling 'get_playlist_items' on ", vid_ids, ": ", e))
-#         # Return NULL in case of error
-#         return(NULL)
-#       }
-#       )
-#     }
-#
-#     # Get video details
-#     n <- length(vid_ids)
-#     message(paste0("Fetching details from n = ", n, " videos on channel_id: ", channel_id, " ..."))
-#     details <- pbapply::pblapply(vid_ids, trycatch_get_video_details, part = part)
-#
-#     return(details)
-#
-#   }else{
-#     return(NULL)
-#   }
-# }
+
+#' FIXED VERSION OF Get Playlist Items
+#'
+#' @param filter string; Required.
+#' named vector of length 1
+#' potential names of the entry in the vector:
+#' \code{item_id}: comma-separated list of one or more unique playlist item IDs.
+#' \code{playlist_id}: YouTube playlist ID.
+#'
+#' @param part Required. Comma separated string including one or more of the
+#' following: \code{contentDetails, id, snippet, status}. Default:
+#' \code{contentDetails}.
+#' @param max_results Maximum number of items that should be returned.
+#' Integer. Optional. Default is 50.
+#' If over 50, all the results are returned.
+#' @param simplify returns a data.frame rather than a list.
+#' @param page_token specific page in the result set that should be
+#' returned, optional
+#' @param video_id  Optional. request should return only the playlist
+#' items that contain the specified video.
+#' @param \dots Additional arguments passed to \code{\link{tuber_GET}}.
+#'
+#' @return playlist items
+#' @import tuber
+#' @export
+#' @references \url{https://developers.google.com/youtube/v3/docs/playlists/list}
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # Set API token via yt_oauth() first
+#'
+#' get_playlist_items(filter =
+#'                        c(playlist_id = "PLrEnWoR732-CN09YykVof2lxdI3MLOZda"))
+#' get_playlist_items(filter =
+#'                        c(playlist_id = "PL0fOlXVeVW9QMO3GoESky4yDgQfK2SsXN"),
+#'                        max_results = 51)
+#' }
+
+get_playlist_items_FIX <- function(filter = NULL, part = "contentDetails",
+                               max_results = 50, video_id = NULL,
+                               page_token = NULL, simplify = TRUE, ...) {
+
+  # if (max_results < 0 || max_results > 50) {
+  #   stop("max_results must be a value between 0 and 50.")
+  # }
+
+  valid_filters <- c("item_id", "playlist_id")
+  if (!(names(filter) %in% valid_filters)) {
+    stop("filter can only take one of the following values: item_id, playlist_id.")
+  }
+
+  if (length(filter) != 1) {
+    stop("filter must be a vector of length 1.")
+  }
+
+  translate_filter <- c(item_id = "id", playlist_id = "playlistId")
+  filter_name <- translate_filter[names(filter)]
+  names(filter) <- filter_name
+
+  querylist <- list(part = part,
+                    maxResults = max(min(max_results, 50), 1),
+                    pageToken = page_token, videoId = video_id)
+  querylist <- c(querylist, filter)
+
+  res <- tuber:::tuber_GET("playlistItems", querylist, ...)
+
+  if (max_results > 50) {
+    page_token <- res$nextPageToken
+    while (is.character(page_token)) {
+      a_res <- tuber:::tuber_GET("playlistItems", list(
+        part = part,
+        playlistId = unname(filter["playlistId"]),
+        maxResults = 50,
+        pageToken = page_token
+      ), ...)
+      res <- c(res, a_res)
+      page_token <- a_res$nextPageToken
+    }
+  }
+
+  if (simplify) {
+    allResultsList <- unlist(res[which(names(res) == "items")], recursive = FALSE)
+    allResultsList <- lapply(allResultsList, unlist)
+    res <-
+      do.call(
+        plyr::rbind.fill,
+        lapply(
+          allResultsList,
+          function(x) as.data.frame(t(x), stringsAsFactors = FALSE)
+        )
+      )
+  }
+
+  res
+}
