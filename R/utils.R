eac086424e0f115e406edf81054ec0f250fd392c
Daniel Thiele <daniel.thiele@fu-berlin.de>
2024-05-18 14:44:23 +0200

    adding set_parentdir for working on server

--- R/utils.R
+++ R/utils.R
@@ -74,3 +74,89 @@ remove_invalid_jsons <- function(dir) {
   }
 }
 
+
+
+
+
+
+#' Set Parent Directory Based on System Information
+#'
+#' This function sets the parent directory based on the system's node name and login information.
+#' It takes specifications for up to three different systems and matches the current system's
+#' information to set the appropriate directory. If no match is found, it sets the directory to
+#' the current working directory.
+#'
+#' @param nodename_A Character. Node name of the first system. Must be provided.
+#' @param login_A Character. Login name of the first system. Must be provided.
+#' @param dir_A Character. Directory path for the first system. Must be provided.
+#' @param nodename_B Character. Node name of the second system. Must be provided.
+#' @param login_B Character. Login name of the second system. Must be provided.
+#' @param dir_B Character. Directory path for the second system. Must be provided.
+#' @param nodename_C Character. Node name of the third system. Optional.
+#' @param login_C Character. Login name of the third system. Optional.
+#' @param dir_C Character. Directory path for the third system. Optional.
+#'
+#' @return A character string representing the parent directory.
+#' If no match is found, returns the current working directory.
+#' If the specified directory does not exist, an error is thrown.
+#'
+#' @examples
+#' \dontrun{
+#' set_parentdir(nodename_A = "node1", login_A = "user1", dir_A = "/path/to/dir1",
+#'               nodename_B = "node2", login_B = "user2", dir_B = "/path/to/dir2")
+#'
+#' set_parentdir(nodename_A = "node1", login_A = "user1", dir_A = "/path/to/dir1",
+#'               nodename_B = "node2", login_B = "user2", dir_B = "/path/to/dir2",
+#'               nodename_C = "node3", login_C = "user3", dir_C = "/path/to/dir3")
+#' }
+#'
+#' @export
+set_parentdir <- function(nodename_A = NULL,
+                          login_A = NULL,
+                          dir_A = NULL,
+                          nodename_B = NULL,
+                          login_B = NULL,
+                          dir_B = NULL,
+                          nodename_C = NULL,
+                          login_C = NULL,
+                          dir_C = NULL
+){
+
+  # Stop if missing specifications
+  if(is.null(nodename_A)) stop("Please provide nodename_A.")
+  if(is.null(login_A)) stop("Please provide login_A.")
+  if(is.null(dir_A)) stop("Please provide dir_A.")
+
+  if(is.null(nodename_B)) stop("Please provide nodename_B.")
+  if(is.null(login_B)) stop("Please provide login_B.")
+  if(is.null(dir_B)) stop("Please provide dir_B.")
+
+  exists("par_dir")
+
+  # Get System info
+  sysinfo <- Sys.info()
+
+  # Set directory if on machine A
+  if(sysinfo[["nodename"]] == nodename_A && sysinfo[["login"]] == login_A)  par_dir <- file.path(dir_A)
+
+  # Set directory if on machine B
+  if(sysinfo[["nodename"]] == nodename_B && sysinfo[["login"]] == login_B)  par_dir <- file.path(dir_B)
+
+  # Evaluate third option if specified
+  if(!is.null(nodename_C) && !is.null(login_C) && !is.null(dir_C)){
+    if(sysinfo[["nodename"]] == nodename_C && sysinfo[["login"]] == login_C) par_dir <- file.path(dir_C)
+  }
+
+  # Set to current working directory if no match
+  if(!exists("par_dir")){
+    warning("No directory specified. Setting current working directory.")
+    par_dir <- getwd()
+  }
+
+  # Error if parent directory invalid
+  if(!dir.exists(par_dir)) stop(paste("Parent directory is invalid:", par_dir))
+
+  # Return
+  return(par_dir)
+}
+
